8392188656306085c565d658056ef8ab8b10e42c
YaoYue <938778575@qq.com>
2022-08-19 15:12:45 +0800

    Format Code

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,28 +1,28 @@
-import {defineUserConfig,} from 'vuepress'
+import { defineUserConfig, } from 'vuepress'
 
-const {defaultTheme} = require('vuepress')
-const {docsearchPlugin} = require('@vuepress/plugin-docsearch')
-const {registerComponentsPlugin} = require('@vuepress/plugin-register-components')
-const {path} = require('@vuepress/utils')
+const { defaultTheme } = require('vuepress')
+const { docsearchPlugin } = require('@vuepress/plugin-docsearch')
+const { registerComponentsPlugin } = require('@vuepress/plugin-register-components')
+const { path } = require('@vuepress/utils')
 export default defineUserConfig({
-    lang: 'zh-CN',
-    title: 'FE Knowledge',
-    description: '这是我的第一个 VuePress 站点',
-    public: 'public',
-    head: [['link', {rel: 'icon', href: '/logo.png'}]],
-    plugins: [
-        docsearchPlugin({
-            // 配置项
-        }),
-        registerComponentsPlugin({
-            components: {
-                Title: path.resolve(__dirname, '../../components/Title.vue'),
-            },
-        }),
-    ],
-    port: 5472,
-    theme: defaultTheme({
-        navbar: [{text: '首页', link: '/'}, {text: 'React', link: '/React/index.html'}],
-        sidebar: [{text: 'React', link: '/React/index.html'}]
-    })
+  lang: 'zh-CN',
+  title: 'FE Knowledge',
+  description: '这是我的第一个 VuePress 站点',
+  public: 'public',
+  head: [ [ 'link', { rel: 'icon', href: '/logo.png' } ] ],
+  plugins: [
+    docsearchPlugin({
+      // 配置项
+    }),
+    registerComponentsPlugin({
+      components: {
+        Title: path.resolve(__dirname, '../../components/Title.vue'),
+      },
+    }),
+  ],
+  port: 5472,
+  theme: defaultTheme({
+    navbar: [ { text: '首页', link: '/' }, { text: 'React', link: '/React/index.html' } ],
+    sidebar: [ { text: 'React', link: '/React/index.html' } ]
+  })
 })
